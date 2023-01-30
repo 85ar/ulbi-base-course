9992eece8fc0815ef9347407c16c8e7fcedbd72a
Artur Valeev <85ar@rambler.ru>
2023-01-30 23:33:42 +0500

    wip1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import PostList from "./components/PostList";
+
+import "./styles/App.css";
+
 function App() {
-  return <div className="App">тест</div>;
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "JavaScript",
+      body: "Description",
+    },
+    {
+      id: 2,
+      title: "JavaScript2",
+      body: "Description2",
+    },
+    {
+      id: 3,
+      title: "JavaScript3",
+      body: "Description3",
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <PostList posts={posts} title="Посты про JS" />
+    </div>
+  );
 }
 
 export default App;
